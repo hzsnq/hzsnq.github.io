5e37dc67412a1b08126eaef4537c203c92664d53
hzsnq <oncwnuCfHIi6EWBy0bUCS64ALE_U@git.weixin.qq.com>
2019-11-28 13:20:38 +0800

    @hzsnq add code

---
+++ build/precache-manifest.a27eb5fe65fe1cb1e928eb82812435f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34c92c9c9a2ea472b4dd9075d7c9d4e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4d2b9e52a8f3f548475",
+    "url": "/static/css/main.4d3a117d.chunk.css"
+  },
+  {
+    "revision": "8a83808158163cc96714",
+    "url": "/static/js/2.a7f526f7.chunk.js"
+  },
+  {
+    "revision": "c4d2b9e52a8f3f548475",
+    "url": "/static/js/main.80152769.chunk.js"
+  },
+  {
+    "revision": "fe5c7aae141f03b42984",
+    "url": "/static/js/runtime-main.8eea71de.js"
+  }
+]);
